b3e670d0b43a9231e21ae85e64d8975c0695dd7f
Nicolas Lenz <nicolas@eisfunke.com>
2023-04-05 00:52:15 +0200

    stuff? no idea tbh

--- src/GraphvizBlock.hs
+++ src/GraphvizBlock.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module GraphvizBlock (graphvizBlock) where
 
 import Data.ByteString.Lazy.UTF8 (fromString)
@@ -5,6 +7,7 @@ import Data.Digest.Pure.SHA (sha256, showDigest)
 import System.Exit (ExitCode (ExitSuccess))
 import System.Process (readProcessWithExitCode)
 import Text.Pandoc.Definition
+import qualified Data.Text as T
 
 graphvizBlock :: Maybe Format -> Block -> IO Block
 graphvizBlock (Just format) (CodeBlock (id, classes, keyvals) content)
@@ -12,28 +15,28 @@ graphvizBlock (Just format) (CodeBlock (id, classes, keyvals) content)
         case format of
           Format "html" ->
               do
-                (ec, out, err) <- readProcessWithExitCode "dot" ["-Tsvg", "-K" ++ layout] content
+                (ec, out, err) <- readProcessWithExitCode "dot" ["-Tsvg", T.unpack $ "-K" <> layout] (T.unpack content)
                 return $ case ec of
                            ExitSuccess -> RawBlock (Format "html")
-                                 ("<div id=\"" ++ id ++ "\" class=\"graphviz\">" ++ out ++ "</div>")
-                           _ -> CodeBlock (id, classes, keyvals) err
+                                 ("<div id=\"" <> id <> (T.pack $ "\" class=\"graphviz\">" <> out <> "</div>"))
+                           _ -> CodeBlock (id, classes, keyvals) (T.pack err)
           Format "latex" ->
               do
                 (ec, out, err) <- readProcessWithExitCode
-                                  "dot2tex" ["--figonly", "--progoptions=-K" ++ layout] content
+                                  "dot2tex" ["--figonly", T.unpack $ "--progoptions=-K" <> layout] (T.unpack content)
                 return $ if length err == 0
-                         then RawBlock (Format "latex") out
-                         else CodeBlock (id, classes, keyvals) err
+                         then RawBlock (Format "latex") (T.pack out)
+                         else CodeBlock (id, classes, keyvals) (T.pack err)
           _ ->
               do
                 (ec, out, err) <- readProcessWithExitCode
-                                  "dot" ["-Tpng", "-K" ++ layout, "-o" ++ filename] content
+                                  "dot" ["-Tpng", T.unpack $ "-K" <> layout, "-o" <> filename] (T.unpack content)
                 return $ case ec of
-                           ExitSuccess -> Para [Image (id, [], []) [Str content] (filename, id)]
-                           _ -> CodeBlock (id, classes, keyvals) err
+                           ExitSuccess -> Para [Image (id, [], []) [Str content] (T.pack filename, id)]
+                           _ -> CodeBlock (id, classes, keyvals) (T.pack err)
               where
-                uniq = ((showDigest . sha256 . fromString) (layout ++ "/" ++ content))
-                filename = "graphviz-" ++ uniq ++ ".png"
+                uniq = ((showDigest . sha256 . fromString) (T.unpack $ layout <> "/" <> content))
+                filename = "graphviz-" <> uniq <> ".png"
     where
       layout =
           case lookup "layout" keyvals of
